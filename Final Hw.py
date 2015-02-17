7b0276dbb011d58e834fdded2477f8569a59eb2c
michmoyn <michmoyn@bu.edu>
2015-02-17 11:44:00 -0500

    ek 128 hw 1

---
+++ Final Hw.py
@@ -0,0 +1,29 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+name_1= input("what is your name?")
+name_2= input("what is your name?")
+print("Hello",name_1)
+print("hello", name_2)
+
+
+# In[ ]:
+
+number= int(input("Give me a number "))
+newnumber= number+3
+newnumber= newnumber*2
+newnumber= newnumber-4
+newnumber= newnumber- 2*number
+final= newnumber+3
+print(final)
+
+
+# In[ ]:
+
+temperature= int(input("What is the temperature in F? "))
+celsius= temperature-32
+celsius= celsius* 5/9
+print("The temperature in degrees celsius is ",celsius )
+
